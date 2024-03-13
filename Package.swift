bbf542fe5fe45dc4682c6a2cf7c21a5b63b053bd
Ariesta <ariesta.work@gmail.com>
2024-03-13 09:22:12 +0700

    Update Dependency

--- Package.swift
+++ Package.swift
@@ -11,11 +11,18 @@ let package = Package(
             name: "ScanQRModule",
             targets: ["ScanQRModule"]),
     ],
+    dependencies: [
+        .package(path: "../PaymentModule"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ScanQRModule"),
+            name: "ScanQRModule",
+            dependencies: [
+                "PaymentModule"
+            ]
+        ),
         .testTarget(
             name: "ScanQRModuleTests",
             dependencies: ["ScanQRModule"]),
